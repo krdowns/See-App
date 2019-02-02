a82471d0f2a7ac88fd96a6a7a0f7ea9923781da5
krdowns <krjdowns16@gmail.com>
2019-02-02 09:36:33 -0800

    still trying to fix heroku issues create

--- public/scripts/createentry.js
+++ public/scripts/createentry.js
@@ -3,14 +3,14 @@ $('#save_entry').on('click', function (e) {
     console.log('clicked')
     e.preventDefault();
     var elements = $('#newEntryForm')[0].elements
-    var title = elements[0].value
-    var content = elements[1].value
+    var content = elements[0].value
+    // var content = elements[1].value
     var author = localStorage.userID
         $.ajax({
             url: '/api/entries',
             method: 'POST',
             data: {
-                title,
+                // title,
                 content,
                 author
             },
